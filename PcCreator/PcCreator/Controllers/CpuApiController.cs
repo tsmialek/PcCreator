719ac4893cf6d930872fd95fbdeba67cba4139b8
tsmialek <tsmialek.1@gmail.com>
2024-02-04 14:38:41 +0100

    Changes: - dodano CpuApiController.cs oraz GpuApiController.cs - dodano CreateApi.cshtml - widok do tworzenia PC wykorzystujący API - zmodyfikowany PcController.cs - dodano metody do obsługi widoku CreateApi.cshtml - zmodyfikowany Index.cshtml - dodano przycisk do wyświetlania widoku CreateApi.cshtml
    Teraz aplikacja umożliwia wyszukiwanie kart graficznych i cpu i filtrowanie ich po nazwie wykorzystując przy tym interfejs API.
    
    ToDo:
    - formularz do dodawania Kart Graficznych i CPU
    - formularz do dodawania CPU i GPU przez API (modyfikacja CpuApiController.cs oraz GpuApiController.cs)

---
+++ PcCreator/PcCreator/Controllers/CpuApiController.cs
@@ -0,0 +1,28 @@
+﻿using Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace PcCreator.Controllers
+{
+    [Route("api/cpus")]
+    [ApiController]
+    public class CpuApiController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CpuApiController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetFiltered(string filterCpu)
+        {
+            return Ok(_context.Cpus
+                .Where(g => g.Name.Contains(filterCpu))
+                .Select(g => new { g.Id, g.Name })
+                .ToList());
+        }
+    }
+}
